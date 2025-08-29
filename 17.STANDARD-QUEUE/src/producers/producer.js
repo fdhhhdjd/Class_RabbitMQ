be3e09ce4a00065f44cd0dc779f020ae7c28f49f
fdhhhdjd <nguyentientai10@gmail.com>
2025-08-29 08:51:35 +0700

    fix: remove comment producers

--- 17.STANDARD-QUEUE/src/producers/producer.js
+++ 17.STANDARD-QUEUE/src/producers/producer.js
@@ -1,26 +1,40 @@
-const RabbitMQ = require("../core/RabbitMQ")
+const RabbitMQ = require("../core/RabbitMQ");
 
 const Producers = async () => {
-    // 1. Normal Queue
-    // const queueName = 'email.send.v1.dev'
-    // const messageQueue = 'Hello everybody'
-    // await RabbitMQ.sendToQueue(queueName, messageQueue)
+  // 1. Normal Queue
+  const queueName = "email.send.v1.dev";
+  const messageQueue = "Hello everybody";
+  await RabbitMQ.sendToQueue(queueName, messageQueue);
 
-    // // 2. Fanout
-    // const exchangeFanout = 'order.commands.v1'
-    // const messageFanout = 'Hello everybody Fanout'
-    // await RabbitMQ.publish(exchangeFanout,"fanout","", messageFanout)
+  // 2. Fanout
+  const exchangeFanout = "order.commands.v1";
+  const messageFanout = "Hello everybody Fanout";
+  await RabbitMQ.publish(exchangeFanout, "fanout", "", messageFanout);
 
-    // 3. Direct
-    // const exchangeDirect = 'email.events.v1'
-    // await RabbitMQ.publish(exchangeDirect, "direct", "info", "Hello direct info");
-    // await RabbitMQ.publish(exchangeDirect, "direct", "error", "Hello direct error");
+  // 3. Direct
+  const exchangeDirect = "email.events.v1";
+  await RabbitMQ.publish(exchangeDirect, "direct", "info", "Hello direct info");
+  await RabbitMQ.publish(
+    exchangeDirect,
+    "direct",
+    "error",
+    "Hello direct error"
+  );
 
-    // 4. Topic
-    const exchangeTopic= 'email.topics.v1'
-    await RabbitMQ.publish(exchangeTopic, "topic", "user.created", "Hello topic user.created");
-    await RabbitMQ.publish(exchangeTopic, "topic", "order.completed", "Hello topic order.completed");
-    
-}
+  // 4. Topic
+  const exchangeTopic = "email.topics.v1";
+  await RabbitMQ.publish(
+    exchangeTopic,
+    "topic",
+    "user.created",
+    "Hello topic user.created"
+  );
+  await RabbitMQ.publish(
+    exchangeTopic,
+    "topic",
+    "order.completed",
+    "Hello topic order.completed"
+  );
+};
 
-Producers()
+Producers();
